557e2bf2dea87a742ef1342bb88ea211dd5f5639
Ching Chan <65863602+themodern@users.noreply.github.com>
2023-10-17 17:34:43 +1100

    add Booking class in the backend

--- HMS/hotel/models.py
+++ HMS/hotel/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+#add new import 
+from django.conf import settings
 # Create your models here.
 # create room and model 
 
@@ -20,3 +21,14 @@ class Room(models.Model):
 # this will display in admin panel if a new rooom obj is added
     def __str__(self):
         return f'{self.number}. {self.categories} with {self.beds} beds for {self.capacity} people'
+    
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user} for {self.room} guests from {self.check_in} to {self.check_out}'
+    
